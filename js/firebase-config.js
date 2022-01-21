49bde1dfa4e05ce6cb3170485b95183d652898d3
Antonio Barbosa <antonio.barbosa@codeminer42.com>
2022-01-21 19:55:54 -0300

    reset config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,13 +4,13 @@
  */
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvlCd0IxXElokpU20tWHnzuiuvDvDhIqU",
-    authDomain: "mastering-security-rules.firebaseapp.com",
-    projectId: "mastering-security-rules",
-    storageBucket: "mastering-security-rules.appspot.com",
-    messagingSenderId: "675379298879",
-    appId: "1:675379298879:web:cff7f356048501b20f6e8a",
-    measurementId: "G-3N1ZTWPPSZ"
+    apiKey: "...",
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
