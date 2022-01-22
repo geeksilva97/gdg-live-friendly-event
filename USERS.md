6095cfe6b3dee8285d9431c22eec2c862e878d37
Antonio Barbosa <antonio.barbosa@codeminer42.com>
2022-01-21 21:37:40 -0300

    add users markdown

---
+++ USERS.md
@@ -0,0 +1,5 @@
+E-mail: test1@test.com
+Senha: 123456
+
+E-mail: test2@test.com
+Senha: 123456
--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,14 +4,15 @@
  */
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "...",
-    measurementId: "..."
-  };
+    apiKey: "AIzaSyAgvazM-PiFlGCCilBVS82N_cQOlVZ2yRE",
+    authDomain: "security-rules-event.firebaseapp.com",
+    projectId: "security-rules-event",
+    storageBucket: "security-rules-event.appspot.com",
+    messagingSenderId: "282667396632",
+    appId: "1:282667396632:web:2eeff9c9fa4c423927dd6d",
+    measurementId: "G-7X0EXQC54F"
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
